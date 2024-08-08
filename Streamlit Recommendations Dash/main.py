7e963f6e7dc0d6a756c7233412f111576330ebb4
Merlin The Great <merlin+newdemo@quix.io>
2024-08-08 15:27:55 +0000

    Edited 'main.py'

--- Streamlit Recommendations Dash/main.py
+++ Streamlit Recommendations Dash/main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 # Streamlit UI
-st.title("Real-time Recommendations Query with Streamlit")
+st.title("Real-time Recommendations Query with Streamlit and Superlinked")
 
 st.markdown("**I shut down the Superlinked server for maintenance, it will be back online Monday 12.08**")
