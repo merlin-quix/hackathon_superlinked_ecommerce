e84b8f339cdac78613187acf4a9b9b63ec42d783
Merlin The Great <merlin+newdemo@quix.io>
2024-08-15 17:22:37 +0000

    Edited 'main.py'

--- MotherDuck Write/main.py
+++ MotherDuck Write/main.py
@@ -1,76 +1,51 @@
-from quixstreams import Application
 import duckdb
-import os
 import logging
-import datetime
+import os
+from quixstreams import Application
+from quixstreams.kafka.configuration import ConnectionConfig
 from dotenv import load_dotenv
-load_dotenv()
 
-mdtoken = os.environ['MOTHERDUCK_TOKEN']
-mddatabase = os.environ['MOTHERDUCK_DATABASE']
-# initiate the MotherDuck connection through a service token through
-con = duckdb.connect(f'md:{mddatabase}?motherduck_token={mdtoken}')
-
+load_dotenv() # for local dev, load env vars from a .env file
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-user_events_table = "user_events"
+# Initialize the Quix Application with the connection configuration
+app = Application(consumer_group="count-consumer-v1e",
+                  auto_offset_reset="earliest")
 
-#      payload = {
-#           "user": user,
-#           "product": product,
-#           "event_type": event_type,
-#           "id": generate_random_event_id(),
-#           "created_at": current_time
-#       }
+input_topic = app.topic(os.getenv("input","processed_data")) # Define the input topic to consume from
+tablename = os.getenv("db_table_name","page_actions") # The name of the table we want to write to
+sdf = app.dataframe(input_topic) # Turn the data from the input topic into a streaming dataframe
 
-def to_duckdb(conn, msg):
-    try:
-        sourcerepo = msg["repo"]
-        reportedtime = msg["day_recorded"]
-        logger.info(f"####### Collecting stats for repo {sourcerepo}")
+con = duckdb.connect("stats.db") # Connect to a persisted DuckDB database on the filesystem
 
-        # Check if the referrals table exists and create it if not
-        table_exists = conn.execute(
-            f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{user_events_table}')").fetchone()[0]
-        if not table_exists:
+try:
+    # Do a basic check if the target table exists and create it if not
+    table_exists = con.execute(
+        f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '\"{tablename}\"')").fetchone()[0]
+    if not table_exists:
+        con.execute(f'''
+            CREATE TABLE "{tablename}" (
+                page_id VARCHAR UNIQUE,
+                count INTEGER
+            );
+        ''')
+except duckdb.CatalogException as e:
+     # If basic check failed, check for catalog error as a backup
+    if "already exists" in str(e):
+        print(f"Table '{tablename}' already exists, skipping creation.")
+    else:
+        raise  # Re-raise the exception if it's not about table existence
 
-            conn.execute(f'''
-                CREATE TABLE {user_events_table} (
-                    user VARCHAR,
-                    product VARCHAR,
-                    event_type INTEGER,
-                    id INTEGER,
-                    created_at TIMESTAMP,
-                );
-            ''')
+def insert_data(con, msg):
+    # Insert data into the DB and if the page_id exists, update the count in the existing row
+    con.execute(f'''
+        INSERT INTO {tablename}(page_id, count) VALUES (?, ?)
+        ON CONFLICT (page_id)
+        DO UPDATE SET count = excluded.count;
+        ''', (msg['page_id'], msg['action_count']))
+    logger.info(f"Wrote record: {msg}")
 
-        for record in msg["referrals"]:
-            conn.execute(f'''
-                INSERT INTO {user_events_table} (user, product, event_type, uniques, day) VALUES (?, ?, ?, ?, ?)
-                ''', (sourcerepo, record['referrer'], record['count'], record['uniques'], reportedtime))
-            logger.info(f"Wrote referral record: {record}")
-
-       
-
-    except Exception as e:
-        logger.info(f"{str(datetime.datetime.utcnow())}: Write failed")
-        logger.info(e)
-
-# Define your application and settings
-app = Application(
-    consumer_group=os.environ['CONSUMER_GROUP_NAME'],
-    auto_offset_reset="earliest",
-)
-
-# Define an input topic with JSON deserializer
-input_topic = app.topic(os.environ['input'], value_deserializer="json")
-
-# Initialize a streaming dataframe based on the stream of messages from the input topic:
-sdf = app.dataframe(topic=input_topic)
-sdf = sdf.update(lambda val: print(f"Received update: {val}"))
-
-# Trigger the write function for any new messages(rows) in the SDF
-sdf = sdf.update(lambda val: to_duckdb(con, val), stateful=False)
+sdf = sdf.update(lambda val: insert_data(con, val), stateful=False)
 
 app.run(sdf)
