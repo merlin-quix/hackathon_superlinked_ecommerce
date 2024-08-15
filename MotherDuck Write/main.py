bc2ea9cbf963798517aaaf68b99c120122e5cad9
Merlin The Great <merlin+newdemo@quix.io>
2024-08-15 17:10:18 +0000

    Edited 'main.py'

--- MotherDuck Write/main.py
+++ MotherDuck Write/main.py
@@ -14,10 +14,7 @@ con = duckdb.connect(f'md:{mddatabase}?motherduck_token={mdtoken}')
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-referralstable = "referral_stats_rolling14days"
-pageviewstable = "pageview_stats_rolling14days"
-dailyviewstable = "totalviews_daybreakdown"
-totalviewstable = "totalviews_rolling14days"
+user_events_table = "user_events"
 
 def to_duckdb(conn, msg):
     try:
@@ -27,11 +24,11 @@ def to_duckdb(conn, msg):
 
         # Check if the referrals table exists and create it if not
         table_exists = conn.execute(
-            f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{referralstable}')").fetchone()[0]
+            f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{user_events_table}')").fetchone()[0]
         if not table_exists:
 
             conn.execute(f'''
-                CREATE TABLE {referralstable} (
+                CREATE TABLE {user_events_table} (
                     repo VARCHAR,
                     referrer VARCHAR,
                     count INTEGER,
@@ -43,80 +40,13 @@ def to_duckdb(conn, msg):
 
         for record in msg["referrals"]:
             conn.execute(f'''
-                INSERT INTO {referralstable} (repo, referrer, count, uniques, day) VALUES (?, ?, ?, ?, ?)
+                INSERT INTO {user_events_table} (repo, referrer, count, uniques, day) VALUES (?, ?, ?, ?, ?)
                 ON CONFLICT (repo, referrer, day) # Need to overwrite rather than insert if same combo of date-repo-referrer already exists
                 DO UPDATE SET count = excluded.count, uniques = excluded.uniques;
                 ''', (sourcerepo, record['referrer'], record['count'], record['uniques'], reportedtime))
             logger.info(f"Wrote referral record: {record}")
 
-        # Check if the pageviews table exists and create it if not
-        table_exists = conn.execute(
-            f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{pageviewstable}')").fetchone()[
-            0]
-        if not table_exists:
-            conn.execute(f'''
-                CREATE TABLE {pageviewstable} (
-                    day TIMESTAMP,
-                    repo VARCHAR,
-                    path VARCHAR,
-                    title VARCHAR,
-                    count INTEGER,
-                    uniques INTEGER,
-                    UNIQUE(repo, path, day)
-                );
-            ''')
-
-        for record in msg["pageviews"]:
-            conn.execute(f'''
-                INSERT INTO {pageviewstable} (day, repo, path, title, count, uniques) VALUES (?, ?, ?, ?, ?, ?)
-                ON CONFLICT (repo, path, day)  # Need to overwrite rather than insert if same combo of date-repo-path already exists
-                DO UPDATE SET title = excluded.title, count = excluded.count, uniques = excluded.uniques;
-                ''', (reportedtime, sourcerepo, record['path'], record['title'], record['count'], record['uniques']))
-
-        # Check if the daily views table exists and create it if not
-        table_exists = conn.execute(
-            f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{dailyviewstable}')").fetchone()[
-            0]
-        if not table_exists:
-            conn.execute(f'''
-            CREATE TABLE {dailyviewstable} (
-                day TIMESTAMP,
-                repo VARCHAR,
-                count INTEGER,
-                uniques INTEGER,
-                UNIQUE(repo, day)
-            );
-            ''')
-
-        for record in msg["views"]["views"]:
-            conn.execute(f'''
-                INSERT INTO {dailyviewstable} (day, repo, count, uniques) VALUES (?, ?, ?, ?)
-                ON CONFLICT (repo, day)  # Need to overwrite rather than insert if same combo of date-repo already exists
-                DO UPDATE SET count = excluded.count, uniques = excluded.uniques;
-                ''', (record['timestamp'], sourcerepo, record['count'], record['uniques']))
-            logger.info(f"Wrote views record: {record}")
-
-        # Create the rolling views table to match the data structure
-        table_exists = conn.execute(
-            f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{totalviewstable}')").fetchone()[
-            0]
-        if not table_exists:
-            conn.execute(f'''
-            CREATE TABLE {totalviewstable} (
-                repo VARCHAR,
-                count INTEGER,
-                uniques INTEGER,
-                day TIMESTAMP,
-                UNIQUE(repo, day)
-            );
-            ''')
-
-        conn.execute(f'''
-        INSERT INTO {totalviewstable} (repo, count, uniques, day) VALUES (?, ?, ?, ?)
-        ON CONFLICT (repo, day)   # Need to overwrite rather than insert if same combo of date-repo already exists
-        DO UPDATE SET count = excluded.count, uniques = excluded.uniques;
-        ''', (sourcerepo, msg["views"]["count"], msg["views"]["uniques"], reportedtime))
-        logger.info(f"Wrote agg-views record for: {sourcerepo}")
+       
 
     except Exception as e:
         logger.info(f"{str(datetime.datetime.utcnow())}: Write failed")
