ae3b18a7ccabd20120fbf55b16bd991b6c604d3d
Merlin The Great <merlin+newdemo@quix.io>
2024-08-20 11:29:58 +0000

    Edited 'main.py'

--- Taipy Dashboard/main.py
+++ Taipy Dashboard/main.py
@@ -1,91 +1,216 @@
-import streamlit as st
-import requests
-import time
+import socket
+import pickle
+import math
+from threading import Thread
+from taipy.gui import Gui, State, invoke_callback, get_state_id
+import numpy as np
 import pandas as pd
-from datetime import datetime
-from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, Range1d
-from bokeh.models.tools import HoverTool
 
-# API endpoint URL
-api_url = "https://flaskwebgateway-c8b2898-demo-superlinkeddemo-main.demo.quix.io//events"
+init_lat = 49.247
+init_long = 1.377
 
-## Function to get data from the API
-def get_data():
-    print(f"[{datetime.now()}] Fetching data from API...")
-    response = requests.get(api_url)
-    data = response.json()
-    df = pd.DataFrame(data)
-    return df
+factory_lat = 49.246
+factory_long = 1.369
 
-# Function to get data and cache it
-@st.cache_data
-def get_cached_data():
-    return get_data()
+diff_lat = abs(init_lat - factory_lat) * 15
+diff_long = abs(init_long - factory_long) * 15
 
-# Streamlit UI
-st.title("Real-time Dashboard Example Using Streamlit and Quix")
-st.markdown("This dashboard reads from a table via an API, which is being continuously updated by a sink process in Quix Cloud. It then displays a dynamically updating Bokeh chart and table underneath.")
-st.markdown("In Quix Cloud, we are:\n * Generating synthetic user logs\n * Streaming the data to Kafka\n * Reading from Kafka and aggregating the actions per page\n * Sinking the page view counts (which are continuously updating) to MotherDuck\n\n ")
-st.markdown("What users could learn: How to read from a real-time source and apply some kind of transformation to the data before bringing it into Streamlit (using only Python)")
+lats_unique = np.arange(init_lat - diff_lat, init_lat + diff_lat, 0.001)
+longs_unique = np.arange(init_long - diff_long, init_long + diff_long, 0.001)
 
-# Placeholder for the bar chart and table
-chart_placeholder = st.empty()
-table_placeholder = st.empty()
+countdown = 20
+periods = 0
+line_data = pd.DataFrame({"Time": [], "Max AQI": []})
 
-# Placeholder for countdown text
-countdown_placeholder = st.empty()
+drone_data = pd.DataFrame(
+    {
+        "Drone ID": [43, 234, 32, 23, 5, 323, 12, 238, 21, 84],
+        "Battery Level": [
+            "86%",
+            "56%",
+            "45%",
+            "12%",
+            "85%",
+            "67%",
+            "34%",
+            "78%",
+            "90%",
+            "100%",
+        ],
+        "AQI": [40, 34, 24, 22, 33, 45, 23, 34, 23, 34],
+        "Status": [
+            "Moving",
+            "Measuring",
+            "Measuring",
+            "Stopped",
+            "Measuring",
+            "Moving",
+            "Moving",
+            "Measuring",
+            "Measuring",
+            "Measuring",
+        ],
+    }
+)
 
-# Main loop
-while True:
-    # Get the data
-    df = get_cached_data()
+HOST = "127.0.0.1"
+PORT = 65432
 
-    # Check that data is being retrieved and passed correctly
-    if df.empty:
-        st.error("No data found. Please check your data source.")
-        break
+layout_map = {
+    "mapbox": {
+        "style": "open-street-map",
+        "center": {"lat": init_lat, "lon": init_long},
+        "zoom": 13,
+    },
+    "dragmode": "false",
+    "margin": {"l": 0, "r": 0, "b": 0, "t": 0},
+}
 
-    # Calculate dynamic min and max scales
-    min_count = df['count'].min()
-    max_count = df['count'].max()
-    min_scale = min_count * 0.99
-    max_scale = max_count * 1.01
+layout_line = {
+    "title": "Max Measured AQI over Time",
+    "yaxis": {"range": [0, 150]},
+}
 
-    # Prepare Bokeh data source
-    source = ColumnDataSource(df)
+options = {
+    "opacity": 0.8,
+    "colorscale": "Bluered",
+    "zmin": 0,
+    "zmax": 140,
+    "colorbar": {"title": "AQI"},
+    "hoverinfo": "none",
+}
 
-    # Create a Bokeh figure without specifying height
-    p = figure(x_range=df['page_id'], height=400, title="Page Counts",
-               y_range=Range1d(start=min_scale, end=max_scale))
+config = {"scrollZoom": False, "displayModeBar": False}
 
-    # Add a hover tool
-    hover = HoverTool()
-    hover.tooltips = [("Page ID", "@page_id"), ("Count", "@count")]
-    p.add_tools(hover)
 
-    # Add bars to the figure
-    p.vbar(x='page_id', top='count', width=0.9, source=source)
+def pollution(lat: float, long: float):
+    """
+    Return pollution level in percentage
+    Pollution should be centered around the factory
+    Pollution should decrease with distance to factory
+    Pollution should have an added random component
 
-    # Style the chart
-    p.xgrid.grid_line_color = None
-    p.yaxis.axis_label = "Count"
-    p.xaxis.axis_label = "Page ID"
-    p.xaxis.major_label_orientation = "vertical"  # Vertical label orientation
+    Args:
+        - lat: latitude
+        - long: longitude
 
-    # Display the Bokeh chart in Streamlit using st.bokeh_chart
-    chart_placeholder.bokeh_chart(p, use_container_width=True)
+    Returns:
+        - pollution level
+    """
+    global countdown
+    return 80 * (0.5 + 0.5 * math.sin(countdown / 20)) * math.exp(
+        -(0.8 * (lat - factory_lat) ** 2 + 0.2 * (long - factory_long) ** 2) / 0.00005
+    ) + np.random.randint(0, 50)
 
-    # Display the dataframe as a table
-    table_placeholder.table(df)
 
-    # Countdown
-    for i in range(1, 0, -1):
-        countdown_placeholder.text(f"Refreshing in {i} seconds...")
-        time.sleep(1)
+lats = []
+longs = []
+pollutions = []
+times = []
+max_pollutions = []
 
-    # Clear the countdown text
-    countdown_placeholder.empty()
+for lat in lats_unique:
+    for long in longs_unique:
+        lats.append(lat)
+        longs.append(long)
+        pollutions.append(pollution(lat, long))
 
-    # Clear the cache to fetch new data
-    get_cached_data.clear()
+data_province_displayed = pd.DataFrame(
+    {
+        "Latitude": lats,
+        "Longitude": longs,
+        "Pollution": pollutions,
+    }
+)
+
+max_pollution = data_province_displayed["Pollution"].max()
+
+
+# Socket handler
+def client_handler(gui: Gui, state_id_list: list):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, _ = s.accept()
+    while True:
+        if data := conn.recv(1024 * 1024):
+            pollutions = pickle.loads(data)
+            print(f"Data received: {pollutions[:5]}")
+            if hasattr(gui, "_server") and state_id_list:
+                invoke_callback(
+                    gui,
+                    state_id_list[0],
+                    update_pollutions,
+                    [pollutions],
+                )
+        else:
+            print("Connection closed")
+            break
+
+
+# Gui declaration
+state_id_list = []
+
+Gui.add_shared_variable("pollutions")
+
+
+def on_init(state: State):
+    state_id = get_state_id(state)
+    if (state_id := get_state_id(state)) is not None and state_id != "":
+        state_id_list.append(state_id)
+    update_pollutions(state, pollutions)
+
+
+def update_pollutions(state: State, val):
+    state.pollutions = val
+    state.data_province_displayed = pd.DataFrame(
+        {
+            "Latitude": lats,
+            "Longitude": longs,
+            "Pollution": state.pollutions,
+        }
+    )
+    # Add an hour to the time
+    state.periods = state.periods + 1
+    state.max_pollutions = state.max_pollutions + [max(state.pollutions)]
+    state.times = pd.date_range(
+        "2020-11-04", periods=len(state.max_pollutions), freq="H"
+    )
+    state.line_data = pd.DataFrame(
+        {
+            "Time": state.times,
+            "Max AQI": state.max_pollutions,
+        }
+    )
+
+
+page = """
+<|layout|columns=1 1 3|
+<|part|class_name=card|
+**Max Measured AQI:**<br/><br/><br/>
+<|{int(data_province_displayed["Pollution"].max())}|indicator|value={int(data_province_displayed["Pollution"].max())}|min=140|max=0|>
+<br/><br/>
+**Average Measured AQI:**<br/><br/><br/>
+<|{int(data_province_displayed["Pollution"].mean())}|indicator|value={int(data_province_displayed["Pollution"].mean())}|min=140|max=0|>
+|>
+
+<|part|class_name=card|
+<|{drone_data}|table|show_all=True|>
+|>
+
+<|part|class_name=card|
+<|{line_data[-30:]}|chart|type=lines|x=Time|y=Max AQI|layout={layout_line}|height=40vh|>
+|>
+|>
+"""
+gui = Gui(page=page)
+
+t = Thread(
+    target=client_handler,
+    args=(
+        gui,
+        state_id_list,
+    ),
+)
+t.start()
+gui.run(run_browser=False)
